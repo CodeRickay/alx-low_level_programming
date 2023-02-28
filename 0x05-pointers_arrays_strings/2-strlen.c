95c69e08ce8004c2018096f83ceaecc8d87f3cbe
CodeRickay <afarifredrick421@gmail.com>
2023-02-27 17:06:26 -0800

    all task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - lenght of string
+ * @s:char
+ * Return:int
+ */
+int _strlen(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+continue;
+												}
+return (i);
+}
